97466607a6a3b6ad14a5ba324817d0a8f60615a8
Leozzin075 <99297345+Leozzin075@users.noreply.github.com>
2022-04-11 20:54:32 -0300

    Update Pessoa.java

--- Projeto/src/projeto/Pessoa.java
+++ Projeto/src/projeto/Pessoa.java
@@ -10,5 +10,12 @@ package projeto;
  * @author ALUNO
  */
 public class Pessoa {
-    
+    protected String nome;
+protected String cpf;
+
+    public Pessoa(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
 }
